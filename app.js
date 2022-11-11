d406960b142fa34d2c73f0aa11b06ec9d77425c9
KyryloPikhno <Mr.Good@i.ua>
2022-11-11 21:31:20 +0200

    practice with fs

--- app.js
+++ app.js
@@ -2,20 +2,24 @@ const fs = require('fs/promises');
 const path = require('path');
 
 const sorter = async (readFolder, writeFolder, gender) => {
-    const folderPath = path.join(__dirname, readFolder);
+    try {
+        const folderPath = path.join(__dirname, readFolder);
 
-    const files = await fs.readdir(folderPath);
+        const files = await fs.readdir(folderPath);
 
-    for (const file of files) {
-        const filePath = path.join(folderPath, file);
+        for (const file of files) {
+            const filePath = path.join(folderPath, file);
 
-        const data = await fs.readFile(filePath);
+            const data = await fs.readFile(filePath);
 
-        const user = JSON.parse(data);
+            const user = JSON.parse(data);
 
-        if (user.gender === gender) {
-            await fs.rename(filePath, path.join(__dirname, writeFolder, file));
+            if (user.gender === gender) {
+                await fs.rename(filePath, path.join(__dirname, writeFolder, file));
+            }
         }
+    } catch (e) {
+        console.log(e)
     }
 };
 
