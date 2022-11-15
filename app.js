8a56fde2715bd88b274d356befa242d8386c0c26
KyryloPikhno <Mr.Good@i.ua>
2022-11-15 22:48:16 +0200

    installed express

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-const fs = require('node:fs')
+const express = require('express');
+
+const app = express()
+
+app.get('/users', (req, res) => {
+    console.log('hi')
+    res.json({user: 'Kirill'})
+    // res.end('hi')
+});
+
+app.listen(4000,()=>{
+    console.log('hi')
+})
+
+
