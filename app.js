589b056a4efaf0f438448c55e4059c2741b2eb31
KyryloPikhno <Mr.Good@i.ua>
2022-11-17 20:30:03 +0200

    added post method

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
 const express = require('express');
 
+const userDB = require('./dataBase/users')
+
 const app = express()
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/',(req,res)=>{
+    res.send('WELCOME')
+})
+
 app.get('/users', (req, res) => {
-    console.log('hi')
-    res.json({user: 'Kirill'})
+    // console.log('hi')
+    // res.json({user: 'Kirill'})
     // res.end('hi')
+    res.json(userDB)
 });
 
-app.listen(4000,()=>{
-    console.log('hi')
+app.get('/users/:id',(req,res)=>{
+    const {id} = req.params
+    res.json(userDB[id])
 })
+
+app.post('/users',(req,res)=>{
+    const userInfo = req.body
+
+    console.log(userInfo);
+
+    userDB.push(userInfo)
+
+    res.status(201).json('created')
+})
+
+app.listen(4000,()=>{
+    console.log('server works')
+})
+
+
+
 
 
