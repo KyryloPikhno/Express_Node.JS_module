8a32f3024233f626a455d0fe4bbd6e8e42d894a9
KyryloPikhno <Mr.Good@i.ua>
2022-11-18 00:30:18 +0200

    added service

--- services/service.js
+++ services/service.js
@@ -1,12 +1,14 @@
 const fs = require("fs/promises");
 const path = require("path");
 
-module.exports ={
-    reader: async () => {
-        const buffer = await fs.readFile(path.join(__dirname, 'dataBase', 'users.json'))
-        return JSON.parse(buffer.toString())
-    },
+const pathToFile = path.join(process.cwd(), 'dataBase', 'users.json');
+
+module.exports = {
     writer: async (users) => {
-        await fs.writeFile(path.join(__dirname, 'dataBase','users.json'),JSON.stringify(users))
+        await fs.writeFile(pathToFile, JSON.stringify(users))
+    },
+    reader: async () => {
+       const buffer = await fs.readFile(pathToFile)
+        return JSON.parse(buffer.toString())
     }
-}
+};
