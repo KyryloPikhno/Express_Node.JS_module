033c342631483b505debc50684a4e923b5081905
KyryloPikhno <Mr.Good@i.ua>
2022-11-17 21:21:06 +0200

    added status

--- app.js
+++ app.js
@@ -9,29 +9,48 @@ app.use(express.urlencoded({extended: true}));
 
 app.get('/',(req,res)=>{
     res.send('WELCOME')
+
+    res.status(201).json('localhost is works')
 })
 
 app.get('/users', (req, res) => {
-    // console.log('hi')
-    // res.json({user: 'Kirill'})
-    // res.end('hi')
-    res.json(userDB)
+    if(!userDB){
+        res.send('users not exist')
+
+        res.status(404).json('error')
+    }else {
+        res.json(userDB)
+
+        res.status(201).json('got all users')
+    }
 });
 
 app.get('/users/:id',(req,res)=>{
     const {id} = req.params
 
-    res.json(userDB[id])
+    if(!userDB[id]|| typeof id==="number"){
+        res.send('user not exists or ID isn^t string')
+
+        res.status(404).json('error')
+    }else {
+        res.json(userDB[id])
+
+        res.status(201).json('got current user')
+    }
 })
 
 app.post('/users',(req,res)=>{
     const userInfo = req.body
 
-    console.log(userInfo);
+    if(userInfo.name && userInfo.age) {
+        userDB.push(userInfo)
 
-    userDB.push(userInfo)
+        res.status(201).json('created')
+    }else {
+        res.send('user dosen^t have current properties')
 
-    res.status(201).json('created')
+        res.status(404).json('error')
+    }
 })
 
 app.put('/users/:userId',(req,res)=>{
