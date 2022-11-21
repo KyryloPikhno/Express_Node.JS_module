55147cf3f1240d2f16f3a35b87915d896c2cc5e5
KyryloPikhno <Mr.Good@i.ua>
2022-11-21 13:41:30 +0200

    added router

---
+++ router/user.router.js
@@ -0,0 +1,89 @@
+const {fileServices} = require("../services");
+const router = require('express').Router();
+
+
+router.get('/', async (req, res) => {
+    const users = await fileServices.reader()
+
+    res.json(users)
+});
+
+router.get('/:id', async (req, res) => {
+    const {id} = req.params
+
+    const users = await fileServices.reader()
+
+    const userById = users.find(user => user.id === +id)
+
+    if(!userById){
+        return res.status(404).json(`User with ${id} not found`)
+    }
+
+    res.json(userById)
+});
+
+router.post('/', async (req, res) => {
+    const userInfo = req.body
+
+    if (userInfo.name.length < 2 || typeof userInfo.name !== 'string') {
+        return res.status(400).json('Wrong name')
+    }
+    if (userInfo.age < 18 || Number.isNaN(+userInfo.age)) {
+        return res.status(400).json('Wrong age')
+    }
+
+    const users = await fileServices.reader()
+
+    const newUser = {
+        name: userInfo.name,
+        age: userInfo.age,
+        id: users[users.length - 1].id + 1
+    };
+
+    users.push(newUser);
+
+    await fileServices.writer(users)
+
+    res.status(201).json(newUser)
+});
+
+router.put('/:userId', async (req, res) => {
+    const newUserInfo = req.body
+
+    const {userId} = req.params
+
+    const users = await fileServices.reader()
+
+    const index = users.findIndex(user => user.id === +userId);
+
+    if(index === -1){
+        return res.status(404).json(`User ${userId} not found`)
+    }
+
+    users[index] = {...users[index], ...newUserInfo}
+
+    await fileServices.writer(users)
+
+    res.status(201).json(users[index])
+});
+
+router.delete('/:userId', async (req, res) => {
+    const {userId} = req.params
+
+    const users = await fileServices.reader()
+
+    const index = users.findIndex(user => user.id === +userId);
+
+    if(index === -1){
+        return res.status(404).json(`User ${userId} not found`)
+    }
+
+    users.splice(index,1)
+
+    await fileServices.writer(users)
+
+    res.sendStatus(204)
+});
+
+
+module.exports = router;
