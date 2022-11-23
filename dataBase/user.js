25d574ca6445c867636608fe7fe49d3f13a387ca
KyryloPikhno <Mr.Good@i.ua>
2022-11-23 15:00:21 +0200

    fix app.js

--- dataBase/user.js
+++ dataBase/user.js
@@ -1,8 +1,8 @@
 const {model, Schema} = require('mongoose');
 
 const userSchema = new Schema({
-    name: {type:String,required:true,default:''},
-    email:{type:String,required:true,default:''}
-})
+    name: {type: String, required: true, default: ''},
+    email: {type: String, required: true, default: '', trim: true, toLowerCase: true},
+});
 
-module.exports = model('User', userSchema)
+module.exports = model('User', userSchema);
