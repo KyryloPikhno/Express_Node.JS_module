1480892ba4b7838689b8a76b986521296af9f0d0
KyryloPikhno <Mr.Good@i.ua>
2022-11-29 19:18:22 +0200

    added validator

--- service/oauth.service.js
+++ service/oauth.service.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const ApiError = require("../error/ApiError");
+const {ACCESS_SECRET,REFRESH_SECRET} = require("../config/config");
 
 module.exports = {
     hashPassword: (password) => bcrypt.hash(password, 10),
@@ -15,12 +16,17 @@ module.exports = {
     },
 
     generateAccessTokenPair: (dataToSign = {}) => {
-        const accessToken = jwt.sign(dataToSign, 'secretWorld', { expiresIn: '15m' });
-        const refreshToken = jwt.sign(dataToSign, 'secrtRefreshWorld', { expiresIn: '30d' });
+        const accessToken = jwt.sign(dataToSign, ACCESS_SECRET,{ expiresIn: '15m' });
+        const refreshToken = jwt.sign(dataToSign, REFRESH_SECRET, { expiresIn: '30d' });
 
         return {
             accessToken,
             refreshToken
         }
+    },
+    checkToken: (token = '',tokenType='accessToken') => {
+
+
+        return jwt.verify(token, tokenType)
     }
 }
