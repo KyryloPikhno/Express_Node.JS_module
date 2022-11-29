96cd8fdb08ebabb607ed392e8c5fe08e2e0f84ac
KyryloPikhno <Mr.Good@i.ua>
2022-11-29 18:00:29 +0200

    added validator

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-const fs = require('node:fs')
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+const userRouter = require('./router/user.router');
+const authRouter = require('./router/auth.router');
+const configs = require('./config/config');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/auth', authRouter);
+app.use('/users', userRouter);
+
+app.get('/', (req, res) => {
+    res.json('WELOCME')
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message || 'Unknown error',
+        status: err.status || 500
+    });
+});
+
+app.listen(4900, async () => {
+    await mongoose.connect('mongodb://localhost:27017/admin');
+    console.log(`Server listen ${4900}`);
+});
